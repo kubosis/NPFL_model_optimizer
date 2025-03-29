6f9223f5eab9b4fac5bd0f2fe28b141ad83f246c
kubosis <kubasukdol@gmail.com>
2025-03-29 11:19:33 +0100

    hotfix

--- NPFL_model_optimizer/data/_image_datasets.py
+++ NPFL_model_optimizer/data/_image_datasets.py
@@ -29,16 +29,19 @@ def rand_bbox(size, lam):
     return x1, y1, x2, y2
 
 class BaseDataset(npfl138.TransformedDataset):
-    def __init__(self, dataset, base_transform, label_str="label"):
+    def __init__(self, dataset, base_transform, label_str="label", label_transform=None):
         super().__init__(dataset)
         self._transform = base_transform
         self._label_str = label_str
+        self._label_transform = label_transform
 
     def transform(self, example):
         image = example["image"]
         image = image.to(torch.float32)
         image = self._transform(image)
         label = example[self._label_str]
+        if self._label_transform is not None:
+            label = self._label_transform(label)
         return image, label  # return an (input, target) pair
 
 class AugmentedDataset(torch.utils.data.Dataset):
